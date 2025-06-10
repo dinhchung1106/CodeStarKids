9ee113836aaedbea955389ff70b6406c7d84b7a8
chung <dangdinhchung98@gmail.com>
2025-06-10 20:30:58 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -608,23 +608,23 @@ $('.wave-btn').append(btnHover);
 
 
   /*----------- 00. Inspect Element Disable ----------*/
-  document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-      return false;
-    }
-  }
+  // document.onkeydown = function (e) {
+  //   if (event.keyCode == 123) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+  //     return false;
+  //   }
+  // }
 
 
   document.querySelectorAll('.scroll-link').forEach(link => {
